61b923e7c436961ebbb2d7252e5a200abe433a70
NamVTCA <145656626+NamVTCA@users.noreply.github.com>
2025-07-05 10:18:13 +0700

    update workflow

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 9999;
 app.get('/', (req, res) => {
   res.send('Hello CI/CD 👋');
 });
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
